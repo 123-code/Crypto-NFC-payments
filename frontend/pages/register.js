7541ab8fd63bdb142ea08d05ae03bb637b4d76ce
123-code <naranjojose256@gmail.com>
2022-10-26 10:52:28 -0500

    SubtleCrypto

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,8 +1,37 @@
-import {useState} from 'react';
+import { useState,useRouter } from 'react';
+import crypto from "crypto";
+//import { SubtleCrypto } from 'subtle-crypto';
 
-export function register(){
+
+
+export default function register(){
+crypto === window.crypto
+let[registered,setregistered] = useState(false);
+let[genkeypair,setgenkeypair] = useState(false);
+let[userpin,setuserpin] = useState("");
+
+
+const generateuserkeys = async()=>{
+     let keypair = await crypto.subtle.generateKey({
+        algorithm:{
+            name: "RSA-OAEP",
+            modulusLength: 4096,
+            publicExponent: new Uint8Array([1, 0, 1]),
+            hash: "SHA-256"},
+            extractable:true,
+            keyUsages:["encrypt", "decrypt"]
+    }).then((key)=>{
+        return key
+    })
+    genkeypair(true);
+}
+
+
     return(
         <>
+        <h2> Sign up with: </h2>
+        <button onClick={generateuserkeys}> Generate Keys </button>
+
         </>
     )
 }
