4b4cd488c6f36a422f71856ac41188c9f9c0a5c6
123-code <naranjojose256@gmail.com>
2022-11-07 10:59:06 -0500

    Cards Added

--- Front-end/front-end/src/Pages/index.js
+++ Front-end/front-end/src/Pages/index.js
@@ -70,6 +70,22 @@ const Pagebody = ()=>{
                 </p>
             </div>
 
+            <div
+                class="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-pointer">
+                <span class="p-5 rounded-full bg-orange-500 text-white shadow-lg shadow-orange-200"><svg
+                        xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="1.5">
+                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                        <polyline points="14 2 14 8 20 8"></polyline>
+                        <line x1="16" y1="13" x2="8" y2="13"></line>
+                        <line x1="16" y1="17" x2="8" y2="17"></line>
+                        <polyline points="10 9 9 9 8 9"></polyline>
+                    </svg></span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Save and spend your digital dollars
+                    </p>
+                <p class="mt-2 text-sm text-slate-500">we are creating a decentralized, permissionless
+                financial app for those who want easy access to their money. Send it as easy as a tap</p>
+            </div>
 
             
 </div>
