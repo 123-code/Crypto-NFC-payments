9f40276ac5a2718cf4f0aff3112cbd8d8f1d8ae5
123-code <naranjojose256@gmail.com>
2022-11-07 19:20:36 -0500

    Public Private Keys

--- Front-end/front-end/src/Components/key_generator.js
+++ Front-end/front-end/src/Components/key_generator.js
@@ -1,8 +1,39 @@
 import { useState } from "react";
+import { ethers } from 'ethers';
 import crypto from "crypto";
-import JSEncrypt, { jsencrypt } from 'jsencrypt';
 
-    const testkeys = () =>{
+const renderbutton =()=> {
+    const Key_generator = ()=>{
+        
+       const id = crypto.randomBytes(32).toString('hex');
+       const privateKey = "0x" + id;
+       console.log(`privatekey:${privateKey}`)
+       const wallet = new ethers.Wallet(privateKey);
+       console.log('your address:'+ wallet.address )
+       
+           }
+
+    const onkeygeneration = async()=>{
+       
+
+        try{
+            Key_generator();
+            
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+    return(
+        <>
+        <button Onclick = {onkeygeneration}> Generate Keys </button>
+        </>
+    )
+}
+
+/*
+
+const testkeys = () =>{
         const encrypt = new JSEncrypt();
         const crypto = new JSEncrypt({default_key_size: 2048});
         const publicprivatekey = {
@@ -20,14 +51,6 @@ import JSEncrypt, { jsencrypt } from 'jsencrypt';
     testkeys()
   
 
-
-
-
-
-
-
-    
-/*
   const [pin,setpin] = useState("");
     const data = props;
     const {publicKey,privateKey} = crypto.generateKeyPairSync("rsa",{
--- Front-end/front-end/src/Pages/index.js
+++ Front-end/front-end/src/Pages/index.js
@@ -87,6 +87,18 @@ const Pagebody = ()=>{
                 financial app for those who want easy access to their money. Send it as easy as a tap</p>
             </div>
 
+
+            <div class="p-10 flex flex-col items-center text-center group   md:lg:xl:border-b hover:bg-slate-50 cursor-pointer">
+                <span class="p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200"><svg
+                        xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="1.5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+                    </svg></span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Admission process Guidance</p>
+                <p class="mt-2 text-sm text-slate-500">Professional Advice for higher education abroad and select the
+                    top institutions worldwide.</p>
+            </div>
             
 </div>
 </div>
@@ -153,9 +165,11 @@ const Home = ()=>{
             </div>
         </div>
 
+       
+
     </div>
 <Pagebody/>
-<How/>
+
    </div>
    <footer className = "bg-cyan-500 ">
     
