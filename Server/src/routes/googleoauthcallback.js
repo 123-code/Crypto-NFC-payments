4a7345bac3230d04705c57d5d0b367915934d91e
123-code <naranjojose256@gmail.com>
2022-12-07 15:09:16 -0500

    tests

--- Server/src/routes/googleoauthcallback.js
+++ Server/src/routes/googleoauthcallback.js
@@ -10,11 +10,12 @@ export const googleOauthCallbackRoute = {
         const oauthUserInfo = await getGoogleuser({code});
         const updateduser = await createGoogleUser({oauthUserInfo});
         const{_id:id,email,info} = updateduser;
-        jwt.sign({id,email,info},process.env.JWT_CECRET,(err,token)=>{
+        jwt.sign({id,email,info},process.env.JWT_SECRET,(err,token)=>{
             if(err) return res.sendStatus(500)
             // redirect to create pin screen. 
             
-            res.redirect(`http://localhost:3001/Login?token=${token}`)
+            res.redirect(`http://localhost:3001/Login`)
+            //http://localhost:3001/Login?token=${token}
         })
     }
-}
+}//http://localhost:3000/
--- Server/src/util/OauthClient.js
+++ Server/src/util/OauthClient.js
@@ -3,5 +3,5 @@ import {google} from 'googleapis';
 export const OauthClient = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    'http://localhost:3000/auth/google/callback',
+    'http://localhost:3000//auth/google/callback',
 )
