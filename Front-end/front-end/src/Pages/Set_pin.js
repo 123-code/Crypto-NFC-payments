b9600afd0edd174deec7090d339f406488743baa
123-code <naranjojose256@gmail.com>
2023-01-19 10:05:59 -0500

    Encryptpin

--- Front-end/front-end/src/Pages/Set_pin.js
+++ Front-end/front-end/src/Pages/Set_pin.js
@@ -1,21 +1,43 @@
 import React,{useState} from 'react';
 import PinInput from "react-pin-input";
 import bcrypt from 'bcryptjs';
+import {useNavigate} from 'react-router-dom';
+
 
 export default function App() {
+  const navigate = useNavigate();
 
-const Encryptpin = ()=>{
-  const salt = bcrypt.genSalt(10,function(err,salt){
- bcrypt.hash(setPIN[0],salt,function(err,hash){
-try{
-  console.log('unhashed:',setPIN[0])
-console.log('hash:',hash)
-}catch(err){
-  console.error(err);
-}
- })
-})
-}
+
+  const checkLength = (str)=>{
+    if(str.length === 4){
+      return true
+    }else{
+      return false
+    }
+  }
+  
+  const Encryptpin = ()=>{
+    try{
+      if(checkLength){
+        const salt = bcrypt.genSalt(10,function(err,salt){
+          bcrypt.hash(setPIN[0],salt,function(err,hash){
+        
+          console.log('unhashed:',setPIN[0])
+        console.log('hash:',hash)
+        
+          })
+        })
+        navigate('/account');
+      }
+      else{
+        throw new Error("Pin must be 4 digits")
+      }
+      
+    }catch(err){
+      console.error(err)
+    }
+  
+  }
 
   const [pin,setpin] = useState({ num1: "", num2: "", num3: "", num4: "" })
   const setPIN = [0,0,0,0]
@@ -32,7 +54,7 @@ console.log('hash:',hash)
         }} 
         />
       </div>
-      <button onClick={Encryptpin}>Encrypt</button>
+      <button onClick={Encryptpin}>OK</button>
       </>
 
   );
